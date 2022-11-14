df2719656d09ae74125caca8d9ac63b674f148b6
Midul-islam-Russell <midulraddy121@gmail.com>
2022-11-14 23:02:33 +0600

    add

--- src/component/Cosmatices/Cosmatices.js
+++ src/component/Cosmatices/Cosmatices.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import { addToDb } from '../../utility/fakedb';
 
 const Cosmatices = (props) => {
-    const { id, name, price } = props.cosmatie
+    const { id, name, price } = props.cosmatie;
+
     const addToCard = (id) => {
-        localStorage.setItem(id ,1)
+        addToDb(id);
     }
     const loaceStorageAdd = (id) => {
         localStorage.setItem(id,1)
     }
+
+
+
+
     return (
         <div style={{
             backgroundColor: "yellow",
             padding: "10px",
-            margin: "10px"
-        }}>
+            margin: "10px"}}>
+            
             <p>Consmatices id: {id}</p>
             <p>Consmatices Name : {name}</p>
             <p>Consmatices Price : {price}</p>
--- src/utility/fakedb.js
+++ src/utility/fakedb.js
@@ -1,2 +1,32 @@
 // use local storage to manage cart data
 
+const addToDb = id => {
+    let shoppingCard = {};
+    
+    //get the shopping card
+    const storedCard = localStorage.getItem(shoppingCard);
+
+    if (storedCard) { 
+        shoppingCard = JSON.parse(storedCard);
+    }
+    else {
+        shoppingCard = {};
+
+    }
+
+
+    // add quantity
+    const quantity = shoppingCard[id];
+    if (quantity) {
+        const newQuantity = quantity + 1;
+        shoppingCard[id] = newQuantity;
+        // localStorage.setItem(id, newQuantity);
+    }
+    else {
+        // localStorage.setItem(id, 1);
+        shoppingCard[id] = 1;
+    }
+    localStorage.setItem('shoppingCard', JSON.stringify(shoppingCard));
+}
+
+export{ addToDb };
